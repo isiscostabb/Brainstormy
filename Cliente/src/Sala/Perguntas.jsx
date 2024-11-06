2833fab18d40b6e8f5ce4670d644bef9304239d4
Isis Costa <isiscostabezbirolo@gmail.com>
2024-11-06 20:07:26 -0300

    verificar clique na resposta

--- Cliente/src/Sala/Perguntas.jsx
+++ Cliente/src/Sala/Perguntas.jsx
@@ -1,12 +1,12 @@
-
 import React, { useState, useEffect } from 'react';
 import { tabelaPerguntas } from '../../../Server/Database/tabelaPerguntas.js';
 import './Perguntas.css';
 
-function Perguntas({ statusPergunta, roomCode }) { 
-  
-  const [perguntaData, setPerguntaData] = useState(null);
-  const [respostasAleatorias, setRespostasAleatorias] = useState([]);
+function Perguntas({ statusPergunta, roomCode }) {
+  const [perguntaData, setPerguntaData] = useState(null); // Estado pergunta
+  const [respostasAleatorias, setRespostasAleatorias] = useState([]); // Estado respostas
+  const [acertou, setAcertou] = useState(false); // Estado acerto
+  const [clique, setClique] = useState(false); // Estado de seleção de resposta
 
   useEffect(() => {
     async function fetchPergunta() {
@@ -23,7 +23,7 @@ function Perguntas({ statusPergunta, roomCode }) {
           pergunta.respIncorreta3
         ];
         
-        // Embaralhar (Fisher-Yates)
+        // Função para embaralhar (Fisher-Yates)
         function embaralhar(array) {
           for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -32,15 +32,33 @@ function Perguntas({ statusPergunta, roomCode }) {
           return array;
         }
 
-        // Embaralha as respostas e define o estado
+        // Embaralha as respostas
         setRespostasAleatorias(embaralhar(respostas));
+        setAcertou(false); // Resetar o estado de acerto para cada rodada
+        setClique(false); // Resetar o estado de clique para cada rodada
       }
     }
     fetchPergunta();
-  }, [statusPergunta, roomCode]); // Atualiza quando statusPergunta ou roomCode muda
+  }, [statusPergunta, roomCode]);
 
   if (!perguntaData) {
     return <p>Carregando...</p>;
+  }
+
+  // Função verificar o clique
+  function verificarClique() {
+    setClique(true); // Define o estado para verdadeiro, indicando que o usuário clicou
+    if (setClique == true) {
+      h1
+    }
+  }
+
+  // Função verificar resposp
+  function verificarResposta(resposta) {
+    verificarClique(); // Marca que o usuário clicou em uma resposta
+    if (resposta === perguntaData.respCorreta) {
+      setAcertou(true); // Define o estado de acerto para verdadeiro
+    }
   }
 
   return (
@@ -49,13 +67,18 @@ function Perguntas({ statusPergunta, roomCode }) {
       <div className='opcoesResposta'>
         {respostasAleatorias.map((resposta, index) => (
           <div key={index} className='linha'>
-            <button className='opcao'>
-              <p className='opcaoTxt'>{resposta}</p> 
+            <button className='opcao' onClick={() => verificarResposta(resposta)}>
+              <p className='opcaoTxt'>{resposta}</p>
             </button>
           </div>
         ))}
       </div>
-      <p>O resultado aparece somente após o final da rodada</p>
+      
+      {acertou && <h1>PARABÉNS</h1>} {/* msg acerto */}
+      {clique &&
+      <div className='selecao'>
+              <p className='pSelecao'>O resultado aparece somente após o final da rodada</p>  {/* msg após clique */}
+      </div>}
     </div>
   );
 }
