93512d36af86a5845f3225ba38a9b15362d46f08
Alexandre Destro <aalexandredestro@gmail.com>
2024-11-17 16:56:27 -0300

    Tela /Ranking ..

--- Cliente/src/Ranking/Ranking.jsx
+++ Cliente/src/Ranking/Ranking.jsx
@@ -26,93 +26,102 @@ function CriarPergunta() {
             </div>
             <div className='esquerdoBaixo'>
                 <div className='primeiroLugar'>
-                   
+                    <div className='linhaPlayer'>
+                         <div className='posicaoRank dRank'> 1º</div>
+                         <div className='usuarioRank dRank'> Usuario</div>
+                         <div className='pontosRank dRank'> 120</div>
+                    </div>
                 </div>
             </div>
         </div>
 
 
         <div className='direito'>
-            <div className='direitoMid'>
+          <div className='direitoTop'>
+               <div className='posicaoRank tRank'> Rank</div>
+               <div className='usuarioRank tRank'> Jogador</div>
+               <div className='pontosRank tRank'> Pontos</div>
+          </div>
+          <div className='direitoMid'>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> Usuario</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 3º</div>
                     <div className='usuarioRank dRank'> Usuario 2</div>
-                    <div className='pontosRank dRank'> Pontos: 100</div>
+                    <div className='pontosRank dRank'> 100</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 4º</div>
                     <div className='usuarioRank dRank'> Usuario 3</div>
-                    <div className='pontosRank dRank'> Pontos: 90</div>
+                    <div className='pontosRank dRank'> 90</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 5º</div>
                     <div className='usuarioRank dRank'> joao</div>
-                    <div className='pontosRank dRank'> Pontos: 80</div>
+                    <div className='pontosRank dRank'> 80</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> pedro</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> carlos</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> mairo</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> jose</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> zacarias</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> afonso</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> aaaa</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> bbbb</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> bffff</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> dasdsad</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> 123123</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                <div className='linhaPlayer'>
                     <div className='posicaoRank dRank'> 2º</div>
                     <div className='usuarioRank dRank'> hhhhhhh</div>
-                    <div className='pontosRank dRank'> Pontos: 120</div>
+                    <div className='pontosRank dRank'> 120</div>
                </div>
                
                
