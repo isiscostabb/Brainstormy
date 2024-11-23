0e7bfb3792d5dd0401b0bb052e0bb21c02b44579
Isis Costa <isiscostabezbirolo@gmail.com>
2024-11-23 20:02:53 -0300

    Design Ranking

--- Cliente/src/Ranking/Ranking.jsx
+++ Cliente/src/Ranking/Ranking.jsx
@@ -1,62 +1,59 @@
+
 import { Link } from 'react-router-dom';
 import Conteiner from '../Conteiner';
 import './Ranking.css';
-
-
 
 function CriarPergunta() {
 
 
   return (
     <>
-     <Link to="/">
+
+        {/* voltar lobby */}
+        <Link to="/">
           <button type="submit" className='sair' >
             <p>Sair da Sala</p>
           </button>
         </Link>
+
         <Conteiner altura={'100vh'} largura={'100vw'}>
+
+        {/* parte direita */}
         <div className='esquerdo'>
             <div className='esquerdoCima'>
-               <img src="coroagirandogife.gif" alt="" className='coroa'/>
+              <img src="coroagirandogife.gif" alt="coroa" className='coroa'/>
+              <div className='primeiroCirculo'>
+                <h2 className='h2PrimeiroCirculo'>1º</h2>
+              </div>
             </div>
             <div className='esquerdoBaixo'>
                 <div className='primeiroLugar'>
-                    <div className='linhaPlayer'>
-                         <div className='posicaoRank dRank'> <p className='pe' >1º</p></div>
-                         <div className='usuarioRank dRank'> <p className='pe pe2' >Usuario</p></div>
-                         <div className='pontosRank dRank'> <p className='pe' >120</p></div>
-                    </div>
+                  <p className='p1User'> USUÁRIO </p>
+                  <p className='p1Pont'> 200 pontos</p>
                 </div>
             </div>
         </div>
 
-
-
-
+        {/* parte direita */}
         <div className='direito'>
           <div className='direitoTop'>
-               <div className='posicaoRank tRank'> <p className='pe'>Rank</p></div>
-               <div className='usuarioRank tRank'> <p className='pe pe2'>Jogador</p></div>
-               <div className='pontosRank tRank'> <p className='pe'>Pontos</p></div>
+               <p className='pCab'> POSIÇÃO </p>
+               <p className='pCab'> JOGADOR </p>
+              <p className='pCab'> PONTUAÇÃO </p>
           </div>
           <div className='direitoMid'>
                <div className='linhaPlayer'>
-                    <div className='posicaoRank dRank'> <p className='pe'>2º</p></div>
-                    <div className='usuarioRank dRank'> <p className='pe pe2'>Usuario</p></div>
-                    <div className='pontosRank dRank'> <p className='pe'> 120</p></div>
+                    <div className='posicaoRank'> <p className='pPosicaoRank'>2º</p></div>
+                    <div className='usuarioRank'> <p className='pUsuarioRank'>USUÁRIO</p></div>
+                    <div className='pontosRank'> <p className='pPontosRank'> 120</p></div>
                </div>
                
             </div>
         </div>
-       
-   
-       
+    
         </Conteiner>
     </>
   );
 }
-
-
-
 
 export default CriarPergunta;
